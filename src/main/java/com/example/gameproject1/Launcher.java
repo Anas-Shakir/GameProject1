dc833b16cd94547790d673c8c7a5da750e78ade7
Anas-Shakir <anasshakir035@gmail.com>
2026-05-06 21:52:07 -0700

    structured the code a little bit

--- src/main/java/com/example/gameproject1/Launcher.java
+++ src/main/java/com/example/gameproject1/Launcher.java
@@ -13,6 +13,8 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.Pane;
+import javafx.animation.TranslateTransition;
+import javafx.util.Duration;
 
 import java.awt.*;
 
@@ -34,72 +36,80 @@ public class Launcher extends Application{
 //        scene 1
         // making scene
 
+        // node 1.1
+            // Label and buttons
+        Label title = new Label("Welcome to the Game Human");
 
+        // properties
+        title.setFont(Font.font("Aerial", FontWeight.BOLD, 24));
+        title.setTextFill(Color.WHITE);
 
+        // node 1.2
+        Button playButton = new Button("Enter the World");
+
+            // properties
+        playButton.setStyle(
+                "-fx-background-color:  #f5f5dc"
+        );
+            // events
+        playButton.setOnMouseEntered(e -> playButton.setStyle("-fx-background-color: #ff7f50"));
+        playButton.setOnMouseExited(e -> playButton.setStyle("-fx-background-color:  #f5f5dc"));
+
+        // node 1
         VBox uiLayerMainScreen = new VBox();
+        uiLayerMainScreen.getChildren().addAll(title, playButton);
+        uiLayerMainScreen.setAlignment(Pos.CENTER);
+        VBox.setMargin(title, new Insets(0,0,50,0));
 
+
+        // root scene 1
         StackPane root = new StackPane();
+        root.getChildren().addAll(uiLayerMainScreen);
+            // properties
         root.setBackground(new Background( new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
         root.setAlignment(Pos.CENTER);
+
 
         Scene s = new Scene(root, 400, 400);
 
 
 //        scene 2
 
-
-
-
-
-//        stages
-
-        // Label and buttons
-        Label title = new Label("Welcome to the Game Human");
-//        Label play = new Label("Play");
-        Button playButton = new Button("I love Aether");
-
-
-        // create the player
-//        Rectangle player = new Rectangle(40,40 , Color.DODGERBLUE);
-
+        // node 1.1
         // adding the player
         javafx.scene.image.Image playerImage = new javafx.scene.image.Image(getClass().getResourceAsStream("/ch1.png"));
         javafx.scene.image.ImageView player = new javafx.scene.image.ImageView(playerImage);
 
+        // properties
         player.setFitWidth(40);
         player.setFitHeight(40);
+        player.setX(0);
+        player.setY(0);
         player.setPreserveRatio(true);
 
 
-        // inner thing properties
-        title.setFont(Font.font("Aerial", FontWeight.BOLD, 24));
-        title.setTextFill(Color.WHITE);
-//        play.setTextFill(Color.WHITE);
-        playButton.setStyle(
-                "-fx-background-color:  #f5f5dc"
-        );
-
-        // events
-        playButton.setOnMouseEntered(e -> playButton.setStyle("-fx-background-color: #ff7f50"));
-        playButton.setOnMouseExited(e -> playButton.setStyle("-fx-background-color:  #f5f5dc"));
-
-        // root things
-        VBox.setMargin(title, new Insets(0,0,50,0));
-//        root.setCenter(play);
-        root.getChildren().addAll(title, playButton);
 
 
         // map screen switching
-        // make the button
+        // node
         Button btdBackToMain = new Button("Go To Main Screen");
+
+        // node 2 Layer 1
+        VBox uilayer2 = new VBox();
+        uilayer2.getChildren().addAll(btdBackToMain);
+        uilayer2.setAlignment(Pos.BOTTOM_CENTER);
+
+        // node 1 Layer 0
+        Pane gameWorld = new Pane();
+        gameWorld.getChildren().addAll(player);
+
+
+        // root scene 2
         StackPane map1Layout = new StackPane();
-
-
-        map1Layout.getChildren().addAll(player, btdBackToMain);
-
-
+        map1Layout.getChildren().addAll(gameWorld, uilayer2);
 
         Scene map1 = new Scene(map1Layout, 400, 300);
+
 
 
         playButton.setOnAction(
@@ -127,6 +137,9 @@ public class Launcher extends Application{
 
             }
         });
+
+
+//        stages
 
         // Stage setting
         primaryStage.setTitle("Game Project");
